d8da27234e5b9954df6677794312782736cf8ee7
Michal <hello@leggy.dev>
2023-01-22 22:06:40 +0000

    Create twitter.js
    Added script

---
+++ twitter.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Following Tab
+// @version      0.1
+// @description  Fuck you twitter
+// @author       Fluffy Bean
+// @match        *://*twitter.com/home*
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant        none
+// ==/UserScript==
+function clickTab (jNode) {
+    try {
+        setTimeout(function(){
+            jNode[0].childNodes[1].childNodes[0].click();
+            console.log("Switched to Following!");
+        }, 500);
+    } catch (error) {
+        console.log("Error!!!!!!", error);
+    };
+};
+
+waitForKeyElements("[role='tablist']", clickTab);

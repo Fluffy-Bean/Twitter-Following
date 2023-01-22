549428566cf53d6a41de641a131c6a3160777fa7
Michal <hello@leggy.dev>
2023-01-22 23:53:45 +0000

    Check on tab status
    If tab is already selected, it'll be ignored as it'll send you back up to the top of the page
    If tab so happens to not be on the homepage, it'll also be ignored as its probably not the correct one

--- twitter.js
+++ twitter.js
@@ -4,20 +4,36 @@
 // @description  Fuck you twitter
 // @author       Fluffy Bean
 // @match        *twitter.com/home*
-// @match        *twitter.com*
+// @match        *twitter.com/
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant        none
 // ==/UserScript==
+
 function clickTab (jNode) {
     try {
+        // Add delay otherwise wont click button
         setTimeout(function(){
-            jNode[0].childNodes[1].childNodes[0].click();
-            console.log("Switched to Following!");
+            // Set tab to variable for easier management
+            var tab = jNode[0].childNodes[1].childNodes[0];
+
+            // DEBUG
+            //console.log(tab.ariaSelected, tab.href);
+
+            // BUG: Clicks tabs of profile, following, etc...
+            // AND check if its not already selected, otherwise yeets to top of page
+            if ((tab.href == 'https://twitter.com/home' || tab.href == '/home') && (tab.ariaSelected != 'true')) {
+                tab.click();
+                console.log("Switched to Following!");
+            //} else {
+            //    console.log("Not home tab");
+            };
         }, 500);
     } catch (error) {
+        // ono, error occured!
         console.log("Error!!!!!!", error);
     };
 };
 
-waitForKeyElements("[role='tablist']", clickTab);
+// When a tab found, clicky clicky
+waitForKeyElements("[role='tablist']", clickTab, false);
